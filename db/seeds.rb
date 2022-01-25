0ccac8715702e75d221353277c4b2613290ab93b
lucasz-ligizynski <lukasz.ligizynski@monterail.com>
2022-01-25 21:56:00 +0100

    Create basic seeds for movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+movies = Movie.create([
+    { title: "Harry Testing", description: "This film is about the Harry T.", duration: 5400},
+    { title: "Lord of Tests", description: "This film is about the Ruby Ring", duration: 10800},
+    { title: "Test on Rails", description: "This film is about kil", duration: 5400}])
