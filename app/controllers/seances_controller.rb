da11b217c527e6db06c2fd6df36e4b1843a2b6f6
lukasz-ligizynski <lukasz.ligizynski@monterail.com>
2022-02-01 20:01:03 +0100

    Create basic seances code

---
+++ app/controllers/seances_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class SeancesController < ActionController::Base
+  def index
+    seances = Seances::Repository.new.find_all
+    render json: Seances::Representer.new(seances).basic
+  end
+
+  def movie_params
+    params.require(:seance).permit(:hall_id, :movie_id, :start_time, :end_time)
+  end
+end
