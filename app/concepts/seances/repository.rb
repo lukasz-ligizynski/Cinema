240d58a99c3d78ae6bf97e4401d2fda3e3ad98d1
lukasz-ligizynski <lukasz.ligizynski@monterail.com>
2022-02-04 08:08:08 +0100

    Change repository of seance

--- app/concepts/seances/repository.rb
+++ app/concepts/seances/repository.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
-module Seances
-  class Repository < ::Base::BaseRepository
-    def initialize(adapter: Seance)
-      super(adapter: adapter)
+module Movies
+  class Repository
+    class << self
+      delegate :find_all,
+               to: :new
+    end
+
+    def find_all
+      Movie.order(:id).all
     end
   end
 end
