4963f4d1dbe6d24dd778988f41de06da8ce94fd0
lukasz-ligizynski <lukasz.ligizynski@monterail.com>
2022-02-04 07:58:50 +0100

    Change repository of Halls

--- app/concepts/halls/repository.rb
+++ app/concepts/halls/repository.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 module Halls
-  class Repository < ::Base::BaseRepository
-    def initialize(adapter: Hall)
-      super(adapter: adapter)
+  class Repository
+    class << self
+      delegate :find_all,
+               to: :new
+    end
+
+    def find_all
+      Hall.order(:id).all
     end
   end
 end
