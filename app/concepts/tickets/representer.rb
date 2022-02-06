fbf364585abbd698ea9fcb5e440b2d0229bf3112
lukasz-ligizynski <lukasz.ligizynski@monterail.com>
2022-02-06 19:45:00 +0100

    Add Basic files for Ticket

---
+++ app/concepts/tickets/representer.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Tickets
+  class Representer
+    attr_reader :tickets
+
+    def initialize(tickets)
+      @tickets = tickets
+    end
+
+    def basic
+      tickets.map do |ticket|
+        {
+          id: ticket.id,
+          seat: ticket.seat,
+          type: ticket.type,
+          price: ticket.price,
+          reservation_id: ticket.reservation_id
+        }
+      end
+    end
+  end
+end
