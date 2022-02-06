2e08cff1caecf926f2e9e829ebc0c62e4a9dccec
lukasz-ligizynski <lukasz.ligizynski@monterail.com>
2022-02-06 19:47:30 +0100

    Add ticket and reservation route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get '/movies', to: 'movies#index'
   get '/halls', to: 'halls#index'
   get '/seances', to: 'seances#index'
+  get '/tickets', to: 'tickets#index'
+  get '/reservations', to: 'reservations#index'
 end
-2
