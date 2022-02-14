37866c08542dd385a59a171cf332159a87d20978
lucasz-ligizynski <lukasz.ligizynski@monterail.com>
2022-02-14 09:25:17 +0100

    Double "ii" typo

--- app/controllers/ticket_desks_controller.rb
+++ app/controllers/ticket_desks_controller.rb
@@ -1,7 +1,7 @@
 class TicketDesksController < ActionController::Base
   def index
     tiicket_desk = TicketDesks::Repository.new.find_all
-    render json: TicketDesks::Representer.new(tiicket_desk).basic
+    render json: TicketDesks::Representer.new(ticket_desk).basic
   end
 
   def ticket_desk_params
