429f4f59c9b3c0a72345895fde256bbbc373d239
lukasz-ligizynski <lukasz.ligizynski@monterail.com>
2022-03-12 10:47:42 +0100

    Rework tests for ticket

---
+++ spec/factories/client.rb
@@ -0,0 +1,6 @@
+# This will guess the client class
+FactoryBot.define do
+  factory :client do
+    id { 1 }
+  end
+end
---
+++ spec/factories/seance.rb
@@ -0,0 +1,6 @@
+# This will guess the seance class
+FactoryBot.define do
+  factory :seance do
+    id { 1 }
+  end
+end
