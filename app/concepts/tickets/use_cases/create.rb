8977f43e823895e8eb893940d8c3f48050afd729
lukasz-ligizynski <lukasz.ligizynski@monterail.com>
2022-04-03 15:51:50 +0200

    Check if seat is empty and book it or take first free

--- app/concepts/tickets/use_cases/create.rb
+++ app/concepts/tickets/use_cases/create.rb
@@ -12,12 +12,24 @@ module Tickets
       def call(params:)
         return unless !params.present? || !params.each.present?
 
-        params[:seat] = Tickets::UseCases::FindEmptySeat.new(params: params).call
+        check_seat(params: params)
         begin
           repository.create(params)
         rescue StandardError => e
           puts "Rescued: #{e.inspectt}"
         end
+      end
+    end
+
+    private 
+
+    def check_seat(params:)
+      if params[:seat].empty?
+        params[:seat] = Tickets::UseCases::FindEmptySeat.new(params: params).call
+      elsif Tickets::UseCases::IsSeatEmpty.new(params: params).call
+        'Set is taken, change seat'
+      else
+        params[:seat]
       end
     end
   end
